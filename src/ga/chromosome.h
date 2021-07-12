f3ababed79123e803dde96c1591a79e33513f116
Miles di Prata <milesdiprata3@gmail.com>
2021-07-12 16:48:13 -0400

    Made Chromosome a struct instead of class

--- src/ga/chromosome.h
+++ src/ga/chromosome.h
@@ -1,37 +1,41 @@
 #ifndef GA_CHROMOSOME_H_
 #define GA_CHROMOSOME_H_
 
-#include <array>
 #include <cstddef>
 #include <iostream>
 #include <string>
+#include <utility>
+#include <vector>
 
 namespace ga {
 
 template <typename T, std::size_t N>
-class Chromosome;
+struct Chromosome;
 
 template <typename T, std::size_t N>
 std::ostream& operator<<(std::ostream& os, const Chromosome<T, N>& chromosome);
 
 template <typename T, std::size_t N>
-class Chromosome {
+struct Chromosome {
  public:
   Chromosome();
+  Chromosome(const Chromosome& chromosome);
+  Chromosome(Chromosome&& chromosome);
   ~Chromosome();
 
-  inline const std::array<T, N>& paramaters() const { return parameters_; }
-  inline std::array<T, N>& paramaters() { return parameters_; }
-
-  friend std::ostream& operator<<<T, N>(std::ostream& os,
-                                        const Chromosome& chromosome);
-
- private:
-  std::array<T, N> parameters_;
+  std::vector<T> params;
 };
 
 template <typename T, std::size_t N>
-Chromosome<T, N>::Chromosome() {}
+Chromosome<T, N>::Chromosome() : params(N) {}
+
+template <typename T, std::size_t N>
+Chromosome<T, N>::Chromosome(const Chromosome& chromosome)
+    : params(chromosome.params) {}
+
+template <typename T, std::size_t N>
+Chromosome<T, N>::Chromosome(Chromosome&& chromosome)
+    : params(std::move(chromosome.params)) {}
 
 template <typename T, std::size_t N>
 Chromosome<T, N>::~Chromosome() {}
@@ -41,8 +45,8 @@ std::ostream& operator<<(std::ostream& os, const Chromosome<T, N>& chromosome) {
   std::string seperator = "";
   os << "<";
 
-  for (const auto& parameter : chromosome.parameters_) {
-    std::cout << seperator << parameter;
+  for (const auto& param : chromosome.params) {
+    std::cout << seperator << param;
     seperator = ", ";
   }
 

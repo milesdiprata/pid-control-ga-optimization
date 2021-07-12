dd300306f48c5592475c1867c3324b850ebb6f6e
Miles di Prata <milesdiprata3@gmail.com>
2021-07-12 16:53:08 -0400

    Initial commit of GA Procedure class

---
+++ src/ga/procedure.cc
@@ -0,0 +1,9 @@
+#include "ga/procedure.h"
+
+namespace ga {
+
+Procedure::Procedure() {}
+
+Procedure::~Procedure() {}
+
+}  // namespace ga
---
+++ src/ga/procedure.h
@@ -0,0 +1,14 @@
+#ifndef GA_PROCEDURE_H_
+#define GA_PROCEDURE_H_
+
+namespace ga {
+
+class Procedure {
+ public:
+  Procedure();
+  ~Procedure();
+};
+
+}  // namespace ga
+
+#endif  // GA_PROCEDURE_H_
